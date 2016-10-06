f6d5bd3702053f84cadca2db5ab3c7e04c21e2fc
Landon Townsend <landon.townsend@gmail.com>
2016-10-06 12:23:21 -0700

    finished helix seek projectile

--- Source/KOA_PROTO/BASE_Projectile.cpp
+++ Source/KOA_PROTO/BASE_Projectile.cpp
@@ -49,12 +49,12 @@ void ABASE_Projectile::Tick( float DeltaTime ) {
 		if (existedTime == 0)
 		{
 			DoFixedSeekMovement(true, DeltaTime, startLocation, TargetLocation, ProjLifeTime,
-				existedTime, 500, 20);
+				existedTime, 1, 20);
 		}
 		else if (existedTime <= ProjLifeTime)
 		{
 			DoFixedSeekMovement(false, DeltaTime, startLocation, TargetLocation, ProjLifeTime,
-				existedTime, 500, 20);
+				existedTime, 1, 20);
 		}
 	}
 	else if (ProjTrajectory == EProjectileTrajectory::PT_HELIXSEEK)
@@ -62,12 +62,12 @@ void ABASE_Projectile::Tick( float DeltaTime ) {
 		if (existedTime == 0)
 		{
 			DoHelixSeekMovement(true, DeltaTime, startLocation, TargetLocation, ProjLifeTime,
-				existedTime, 500, 100, 20);
+				existedTime, 1, 1, 20);
 		}
 		else if (existedTime <= ProjLifeTime)
 		{
 			DoHelixSeekMovement(false, DeltaTime, startLocation, TargetLocation, ProjLifeTime,
-				existedTime, 500, 100, 20);
+				existedTime, 1, 1, 20);
 		}
 	}
 	if (existedTime > ProjTimeBeforeDelete)
@@ -145,6 +145,7 @@ void ABASE_Projectile::DoFixedSeekMovement(bool firstFrame, float DeltaSeconds, 
 		this->SetActorLocation(startPos);
 	}
 	FVector currentPos = this->GetActorLocation();
+	float totalLength = (startPos - targetPos).Size();
 	//Create the vectors that will make the x and y axis of a new coordinate system, for simplification purposes
 	//The x axis will be in the direction of the line from our current position to the targetPos
 	FVector xVec = (targetPos - currentPos);
@@ -181,11 +182,16 @@ void ABASE_Projectile::DoHelixSeekMovement(bool firstFrame, float DeltaSeconds, 
 		this->SetActorLocation(startPos);
 	}
 	FVector currentPos = this->GetActorLocation();
+	float totalLength = (startPos - targetPos).Size();
 	//Create the vectors that will make the x and y axis of a new coordinate system, for simplification purposes
 	//The x axis will be in the direction of the line from our current position to the targetPos
 	FVector xVec = (targetPos - currentPos);
 	xVec.Normalize();
-	FVector yVec = FVector::CrossProduct(xVec, FVector(1, 0, 0));
+	FVector zVec = FVector::CrossProduct(xVec, velocity.GetSafeNormal());
+	if (zVec == FVector::ZeroVector)
+		zVec = FVector::CrossProduct(xVec, FVector(1, 0, 0));
+	zVec.Normalize();
+	FVector yVec = FVector::CrossProduct(xVec, zVec);
 	yVec.Normalize();
 
 	//Get our y velocity in our new coordinate system;
@@ -194,17 +200,21 @@ void ABASE_Projectile::DoHelixSeekMovement(bool firstFrame, float DeltaSeconds, 
 	//If it's the first frame, set the y velocity to add a "curve" to the trajectory
 	if (firstFrame)
 	{
-		yVel = FMath::FRandRange(-startVelMax, startVelMax);
+		yVel = FMath::FRandRange(-startVelMax, startVelMax) * totalLength / totalTime;
+		spin = FMath::FRandRange(-spinMax, spinMax) / totalTime;
 	}
 
 	//Set our x velocity based on the distance to the target and the remaining time
 	float xVel = (targetPos - currentPos).Size() / (totalTime - elapsedTime + VERY_SMALL_VALUE);
 
+	//Set our z velocity based on spin and time left
+	float zVel = ((totalTime - elapsedTime) / totalTime) * yVel * spin / 10;
+
 	//Dampen y velocity
 	yVel /= FMath::Pow(1 + damping, DeltaSeconds);
 
 	//convert our new coordinate system velocity back to the world coordinate system, and store it in velocity
-	velocity = xVec * xVel + yVec * yVel;
+	velocity = xVec * xVel + yVec * yVel + zVec * zVel;
 	//apply velocity
 	this->SetActorLocation(currentPos + velocity * DeltaSeconds);
 }

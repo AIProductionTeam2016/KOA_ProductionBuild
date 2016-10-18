e6ceff03d1be183e0cd0e6b181c1c28cc5c71adb
Landon Townsend <landon.townsend@gmail.com>
2016-10-18 16:00:18 -0700

    added squiggly and helix projectile classes, reparented the BP classes to those

---
+++ Source/KOA_PROTO/AMTA_Projectile_HelixSeek.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BASE_Projectile.h"
+#include "AMTA_Projectile_HelixSeek.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KOA_PROTO_API AAMTA_Projectile_HelixSeek : public ABASE_Projectile
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/KOA_PROTO/AMTA_Projectile_Squiggly.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BASE_Projectile.h"
+#include "AMTA_Projectile_Squiggly.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KOA_PROTO_API AAMTA_Projectile_Squiggly : public ABASE_Projectile
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
